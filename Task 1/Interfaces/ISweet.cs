081556864281fb7f249624fe7ebc8e9fc1a42a49
CarryBerry <31474124+CarryBerry@users.noreply.github.com>
2017-10-18 18:40:01 +0300

    Добавьте файлы проекта.

---
+++ Task 1/Interfaces/ISweet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task_1.Classes;
+
+namespace Task_1.Interfaces
+{
+    interface ISweet
+    {
+        double Weight { get; }
+        double SugarContent { get; }
+
+        Shape Shape { get; }
+    }
+}
